e8a715a47afbdf3cf3657013e92f8238aa70eb12
typepatrol <ravindar3350@gmail.com>
2025-08-26 16:41:37 +0530

    Update version.test.ts

--- src/test/version.test.ts
+++ src/test/version.test.ts
@@ -7,6 +7,7 @@ describe('version()', () => {
   test('TESTS cases', () => {
     for (const { value, expectedValidate, expectedVersion } of TESTS) {
       try {
+        // @ts-expect-error testing invalid input
         const actualVersion = version(value);
 
         assert.ok(expectedValidate, `version(${value}) should throw`);
